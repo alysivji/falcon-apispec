da172c90710e8af64e0f6c76c9683ad46fe0170c
André Santos <andreas.bbs@gmail.com>
2022-12-08 02:50:50 +0000

    Remove commented code

--- falcon_apispec/falcon_plugin.py
+++ falcon_apispec/falcon_plugin.py
@@ -18,32 +18,6 @@ class FalconPlugin(BasePlugin):
     def _get_valid_methods(self):
         return set(VALID_METHODS[self._spec.openapi_version.major])
 
-    # def _generate_resource_uri_mapping(self, app, resource):
-    #     valid_methods = self._get_valid_methods()
-    #     routes_to_check = copy.copy(app._router._roots)
-
-    #     mapping = {}
-    #     for route in routes_to_check:
-    #         uri = route.uri_template
-    #         resource = route.resource
-    #         mapping[resource] = {
-    #             "uri": uri,
-    #             "methods": {}
-    #         }
-
-    #         if route.method_map:
-    #             for method_name, method_handler in route.method_map.items():
-    #                 if method_handler.__module__ == "falcon.responders":
-    #                     continue
-    #                 if method_name.lower() not in valid_methods:
-    #                     continue
-    #                 if method_name.lower() not in valid_methods:
-    #                     continue
-    #                 mapping[resource]["methods"][method_name.lower()] = method_handler
-
-    #         routes_to_check.extend(route.children)
-    #     return mapping
-
     def _generate_resource_uri(self, resource):
         valid_methods = self._get_valid_methods()
         routes_to_check = copy.copy(self._app._router._roots)
@@ -70,14 +44,11 @@ class FalconPlugin(BasePlugin):
 
     def path_helper(self, operations, resource, base_path=None, **kwargs):
         """Path helper that allows passing a Falcon resource instance."""
-        #resource_uri_mapping = self._generate_resource_uri_mapping(self._app, resource)
         resource_uri = self._generate_resource_uri(resource)
-        #if resource not in resource_uri_mapping:
         if not resource_uri:
             raise APISpecError("Could not find endpoint for resource {0}".format(resource))
 
         operations.update(yaml_utils.load_operations_from_docstring(resource.__doc__) or {})
-        #path = resource_uri_mapping[resource]["uri"]
         path = resource_uri["uri"]
 
         if base_path is not None:
@@ -86,7 +57,6 @@ class FalconPlugin(BasePlugin):
             base_path = '/' + base_path.strip('/')
             path = re.sub(base_path, "", path, 1)
 
-        #methods = resource_uri_mapping[resource]["methods"]
         methods = resource_uri["methods"]
 
         for method_name, method_handler in methods.items():
